717a8a5cc59e982965bbe67b174a95d785eb0869
Khea Chang <kheacyc@gmail.com>
2021-04-26 20:35:35 +0000

    function returns head of array

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`🛑🛑🛑 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`✅✅✅ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (arr) => {
+  return console.log(arr[0]);
+}
+
+/*
+TEST CASES:
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+*/
