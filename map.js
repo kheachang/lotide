99a261614e5597a32da2584ac5b2477a4e1348d1
Khea Chang <kheacyc@gmail.com>
2021-04-29 20:45:35 +0000

    added comments

--- map.js
+++ map.js
@@ -3,10 +3,11 @@ const words = ["ground", "control", "to", "major", "tom"];
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
+    //for every item, run the callback function and push into a new array
     results.push(callback(item));
   }
   return results;
 }
 
 // TEST:
-// console.log(map(words, word => word[0]))
+// console.log(map(words, word => word[0])) 
