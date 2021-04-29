6f032d72176799a17ee8c885a03f043cba7b8f92
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-04-29 15:05:22 -0700

    added comments

--- findKey.js
+++ findKey.js
@@ -9,11 +9,12 @@ const findKey = function(obj, callback) {
   }
 }
 
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },  
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+// //TEST CASE:
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+// //   "Akaleri":   { stars: 3 },
+// //   "noma":      { stars: 2 },
+// //   "elBulli":   { stars: 3 },  
+// //   "Ora":       { stars: 2 },
+// //   "Akelarre":  { stars: 3 }
+// // }, x => x.stars === 2)) // => "noma"
