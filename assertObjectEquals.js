5555e11606a62304e349c1de72960515037d0781
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-05-01 11:30:50 -0700

    wrote test function

--- assertObjectEquals.js
+++ assertObjectEquals.js
@@ -6,23 +6,56 @@ const assertArraysEqual = (firstArray, secondArray) => {
   }
 };
 
-const assertObjectEqual = (actual, expected) => {
-  for (const actualKey in actual) {
-    for (const expectedKey in expected) {
-      //return false: if the key are not the same 
-      //return false: if the values are not the same
-      if (actualKey !== expectedKey) {
-        return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`
+const eqArrays = (firstArray, secondArray) => {
+  for (let i = 0; i < firstArray.length; i++) {
+    for (let j = 0; j < secondArray.length; j++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
       }
     }
   }
-  return `✅✅✅ Assertion Passed: ${actual} === ${expected}.`
+  return true;
+}
+const eqObjects = function(object1, object2) {
+  let object1key = Object.keys(object1);
+  let object2key = Object.keys(object2);
+  if (object1key.length !== object2key.length) {
+    // console.log("returning becus length is not equal");
+    return false;
+  }
+  for (const value of object1key) {
+    //test the arrays
+    if (Array.isArray(object1[value]) && Array.isArray(object2[value])) {
+      if (! eqArrays(object1[value], object2[value])) {
+        // console.log('returning becus eqArrays is false');
+        return false
+      }
+    } else {
+      //if not arrays, test the values
+      if (object1[value] !== object2[value]) {
+        // console.log(object1[value])
+        // console.log(object2[value])
+  
+        // console.log('false becus val 1 and 2 not equal', value);
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectEqual = (actual, expected) => {
+  if (eqObjects(actual, expected) === false) {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`
+  } else {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}.`
+  }
 }
 
+/*TEST CASES
 const object1 = {a: 1, b: 3, c: 3}
 const object2 = {a: 1, b: 3, c: 3}
-const object3 = {a: 1, b: 5, c: 3}
+const object3 = {a: 1, b: 5}
 
 console.log(assertObjectEqual(object1, object2))
-// || 
-// actual['actualKey'] !== expected['expectedKey'] || 
+*/
