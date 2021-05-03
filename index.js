13cc14b8919790f8f50d60d29e4c466ff8d51f59
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-05-03 15:16:32 -0700

    exported functions into an object

---
+++ index.js
@@ -0,0 +1,41 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectEquals = require('./assertObjectEquals');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const main = require('./main');
+const map = require('./map');
+const modulecheck = require('./modulecheck');
+const mymodule = require('./mymodule');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle, 
+  assertArraysEqual,
+  assertEqual,
+  assertObjectEquals,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  main,
+  map,
+  without,
+  takeUntil,
+  mymodule,
+  modulecheck
+};
