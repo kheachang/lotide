996805df6e380447e29e76698017b67a19a18468
Khea Chang <kheachang@Kheas-MacBook-Air.local>
2021-05-03 20:24:30 -0700

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,19 @@
-const eqArrays = function(firstArray, arr2) {
-  if (firstArray.length !== arr2.length) {
-    return false;
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false
   }
-  for(let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== arr2[i]) {
-      return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
     }
   }
   return true;
 }
 
+
 module.exports = eqArrays;
 
 
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
